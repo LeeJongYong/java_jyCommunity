248d30059bf3adcd81849a057db6cdaec89c8d6c
teddy <jongyong924@gmail.com>
2024-07-17 20:54:17 +0900

    jy_community *done - 프로젝트 생성

---
+++ src/main/java/com/example/jycom/JycomApplication.java
@@ -0,0 +1,13 @@
+package com.example.jycom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JycomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JycomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jycom/JycomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jycom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JycomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
