5b2c30caca486a44165d562b8c4f71cc7e8f0617
teddy924 <jongyong924@gmail.com>
2024-07-23 21:32:39 +0900

    jy_community *done - thymeleaf 적용

--- src/main/java/com/example/jycom/domain/member/controller/MemberController.java
+++ src/main/java/com/example/jycom/domain/member/controller/MemberController.java
@@ -2,18 +2,30 @@ package com.example.jycom.domain.member.controller;
 
 import com.example.jycom.domain.member.domain.Member;
 import com.example.jycom.domain.member.service.MemberService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MemberController {
 
     private final MemberService memberService;
 
+    private static final Logger log = LogManager.getLogger(MemberController.class);
+
     // 생성자 주입
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(){
+        log.info("index 진입");
+        return "content/board";
     }
 
     public void signIn(){
